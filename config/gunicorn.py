7d2cd1a2a07c6f36fa18f16cea140b48278a4bd3
Peith Vergil <peith.vergil@gmail.com>
2015-12-08 22:27:49 +0800

    Add gunicorn settings.

---
+++ config/gunicorn.py
@@ -0,0 +1,7 @@
+bind = '127.0.0.1:8000'
+user = 'peith'
+group = 'peith'
+workers = 3
+loglevel = 'error'
+errorlog = '/home/peith/logs/bugtrax_gunicorn_errorlog'
+accesslog = '/home/pieth/logs/bugtrax_gunicorn_accesslog'
