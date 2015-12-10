2222270c6a134d0c5edc082fbb74e6dedff7afc3
Peith Vergil <peith.vergil@gmail.com>
2015-12-10 09:20:06 +0800

    Add values for the media and static root directories settings.

--- config/settings/prod_sample.py
+++ config/settings/prod_sample.py
@@ -17,6 +17,16 @@ from .base import *
 # Turn off debug mode in production.
 DEBUG = False
 
+for template_engine in TEMPLATES:
+    template_engine['OPTIONS']['debug'] = False
+
 # SECURITY WARNING:
 # Provide a different secret key in production.
 SECRET_KEY = 't#wk_i&_py2ck1#n42)_fnfxl_pol0n&n#jhuh60yh^!z6kg4e'
+
+ALLOWED_HOSTS = [
+    'bugtrax.com', 'www.bugtrax.org'
+]
+
+MEDIA_ROOT = '/home/bugtrax/public/bugtrax/media/'
+STATIC_ROOT = '/home/bugtrax/public/bugtrax/static/'
